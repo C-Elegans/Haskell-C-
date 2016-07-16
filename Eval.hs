db58e7ab597bdb03bba3fc2d51d99dfb4b9e1986
C-Elegans <mtnolan2640@gmail.com>
2016-07-16 17:52:24 -0400

    Add comparison operations to the evaluator

--- Eval.hs
+++ Eval.hs
@@ -1,11 +1,26 @@
 module Eval where
 import Parse 
 
-funcop :: Integral a => OP -> a -> a -> a
+funcop ::  OP -> Integer -> Integer -> Integer
 funcop Plus = (+)
 funcop Minus = (-)
 funcop Mul = (*)
 funcop Div = div
+funcop Eq = apply (==)
+funcop Ne = apply (/=)
+funcop Gt = apply (>)
+funcop Lt = apply (<)
+funcop Le = apply (<=)
+funcop Ge = apply (>=)
+
+apply f l r =
+    let b = f l r
+    in boolToInt b
+
+boolToInt :: Bool -> Integer
+boolToInt b
+        | b == True = 1
+        | otherwise = 0
 
 eval :: Tree -> Integer
 eval (Num x) = x
