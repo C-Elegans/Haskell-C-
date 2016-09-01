f35fc55603b11e047ec39d85ae3821b59fe4fb9c
C-Elegans <mtnolan2640@gmail.com>
2016-09-01 09:40:30 -0400

    Add rand() and srand(int)

--- include/stdlib.h
+++ include/stdlib.h
@@ -3,3 +3,5 @@ int div(int a,int b);
 int atoi(char* str);
 int brk(void* addr);
 void* sbrk(int increment);
+void srand(int seed);
+int rand();
--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 int glob2;
 
 void main(int i){
-    int* ptr;
-    int* ptr2;
-    ptr = sbrk(4);
-    ptr2 = sbrk(4);
-    print_hex((int)ptr);
-    print_hex((int)ptr2);
+    int i;
+    i=0;
+    while(i<16){
+        print_hex(rand()&0xff);
+        i = i+1;
+    }
 }
